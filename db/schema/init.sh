793c00b50419ba595b85cd6d1ee9a133beb5245d
msl-ltd <msl-ltd@outlook.jp>
2021-07-14 00:43:42 +0900

    dbスキーマ定義ファイルを相対パス指定していてDockerコンテナシェルエラーになっていたので絶対パスに変更した

--- db/schema/init.sh
+++ db/schema/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/bin/psql -p 5432 -U postgres  << _EOF
+/usr/bin/psql -p 5432 -U postgres -v SCHEMA_DIR=$(cd $(dirname $0); pwd)  << _EOF
 
-\i ./ddl-m-class.sql
-\i ./dml-m-class.sql
-\i ./ddl-t_payee.sql
-\i ./ddl-t-expense.sql
+\i :SCHEMA_DIR/ddl-m-class.sql
+\i :SCHEMA_DIR/dml-m-class.sql
+\i :SCHEMA_DIR/ddl-t_payee.sql
+\i :SCHEMA_DIR/ddl-t-expense.sql
 
 _EOF
